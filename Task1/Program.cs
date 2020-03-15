722ddc2320696ea6ab50546ee57ba356036bfd5d
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-03-15 03:13:14 +0300

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Type dateTimeProperties = typeof(DateTime);
+            PropertyInfo[] propertyInfos = dateTimeProperties.GetProperties();
+
+            foreach(PropertyInfo pi in propertyInfos)
+            {
+                Console.WriteLine($"{pi}");
+            }
+        }
+    }
+}
