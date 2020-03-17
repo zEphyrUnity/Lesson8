915e40749bbf592918046f6fe1907fc1918c1a90
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-03-18 00:08:10 +0300

    Add files via upload

---
+++ Task2/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(161, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 64);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Task3/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+namespace BelieveOrNotBelieve
+{
+    public partial class Form1 : Form
+    {
+        const int mbyte = 8388608;
+
+        // База данных с вопросами
+        TrueFalse database;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Обработчик пункта меню New
+        private void miNew_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                database = new TrueFalse(sfd.FileName);
+                database.Add("Введите вопрос", true);
+                database.Save();
+                nudNumber.Minimum = 1;
+                nudNumber.Maximum = 1;
+                nudNumber.Value = 1;
+            };
+        }
+
+        // Обработчик пункта меню Open
+        private void miOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                var openedFile = new FileInfo(ofd.FileName);
+                if (openedFile.Length > mbyte)
+                {
+                    MessageBox.Show("Слишком большой файл");
+                    return;
+                }
+                
+                var fileExtension = Path.GetExtension(ofd.FileName);
+                if(Path.GetExtension(ofd.FileName) != ".dat")
+                {
+                    MessageBox.Show("Файл должен иметь расширение .dat");
+                    return;
+                }
+
+                try
+                {
+                    database = new TrueFalse(ofd.FileName);
+                    database.Load();
+                    nudNumber.Minimum = 1;
+                    nudNumber.Maximum = database.Count;
+                    nudNumber.Value = 1;
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show($"Нельзя открыть пустую базу!\n\n {exc.Message}", "Ошибка открытия файла");
+                }
+            }
+        }
+
+        // Обработчик пункта меню Save
+        private void miSave_Click(object sender, EventArgs e)
+        {
+            if (database != null) database.Save();
+            else MessageBox.Show("База данных не создана");
+        }
+
+        // Обработчик пункта меню Exit
+        private void miExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Обработчик кнопки Добавить
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (database == null)
+            {
+                MessageBox.Show("Создайте новую базу данных", "Сообщение");
+                return;
+            }
+
+            database.Add(tboxQuestion.Text, cboxTrue.Checked);
+            nudNumber.Maximum = database.Count;
+            nudNumber.Value = database.Count;
+            tboxQuestion.Text = "Введите вопрос";
+        }
+
+        // Обработчик кнопки Удалить
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (nudNumber.Maximum == 1 || nudNumber.Maximum == 0 || database == null) return;
+            database.Remove((int)nudNumber.Value);
+            nudNumber.Maximum--;
+            if (nudNumber.Value > 1) nudNumber.Value = nudNumber.Value;
+        }
+
+        // Обработчик кнопки Сохранить (вопрос)
+        private void btnSaveQuest_Click(object sender, EventArgs e)
+        {
+            if(database == null)
+            {
+                MessageBox.Show("Не создана база данных", "Ошибка сохранения");
+                return;
+            }
+
+            database[(int)nudNumber.Value - 1].text = tboxQuestion.Text;
+            database[(int)nudNumber.Value - 1].trueFalse = cboxTrue.Checked;
+        }
+
+        // Обработчик события изменения значения numericUpDown
+        private void nudNumber_ValueChanged(object sender, EventArgs e)
+        {
+            if (database == null || nudNumber.Value > database.Count || nudNumber.Value < 1) return;
+            
+            tboxQuestion.Text = database[(int)nudNumber.Value - 1].text;
+            cboxTrue.Checked = database[(int)nudNumber.Value - 1].trueFalse;
+        }
+
+        private void aboutProgramm_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Название: Верю - Не верю Database\n\nАвтор:       zEphyr\n\n" +
+                "Версия      0.0001\n\nGPLv3 (GNU General Public License Version 3)\n\n" +
+                "Дата лицензии: 14.03.2020", "О программе");
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(database.Count.ToString());
+                database.SaveAs(sfd.FileName);
+            };
+        }
+    }
+}
+
